a5d969cac61708d3ef5d4d7c51f8b5070e0c6e8e
alcaidet <63008793+alcaidet@users.noreply.github.com>
2022-04-13 15:01:09 -0400

    fix: refactor size unit tests; added variant unit tests

--- tools/ui-components/src/button/button.test.tsx
+++ tools/ui-components/src/button/button.test.tsx
@@ -41,19 +41,51 @@ describe('Button', () => {
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  it('should add text-lg class when Button size is large', () => {
+  it('should set appropriate classes when Button size is large', () => {
+    const classList = 'px-4 py-2.5 text-lg';
     render(<Button size='large'>Hello world</Button>);
 
     expect(screen.getByRole('button', { name: /hello world/i })).toHaveClass(
-      'text-lg'
+      classList
     );
   });
 
-  it('should add appropriate classes when Button size is small', () => {
+  it('should set appropriate classes when Button size is small', () => {
+    const classList = 'px-2.5 py-1 text-sm';
     render(<Button size='small'>Hello world</Button>);
 
     expect(screen.getByRole('button', { name: /hello world/i })).toHaveClass(
-      'text-sm'
+      classList
+    );
+  });
+
+  it('should set appropriate classes when Button variant is danger', () => {
+    const classList = [
+      'border-default-foreground-danger',
+      'bg-default-background-danger',
+      'text-default-foreground-danger',
+      'hover:bg-default-background-danger-hover',
+      'hover:text-default-foreground-danger-hover'
+    ].join(' ');
+    render(<Button variant='danger'>Hello world</Button>);
+
+    expect(screen.getByRole('button', { name: /hello world/i })).toHaveClass(
+      classList
+    );
+  });
+
+  it('should set appropriate classes when Button variant is info', () => {
+    const classList = [
+      'border-default-foreground-info',
+      'bg-default-background-info',
+      'text-default-foreground-info',
+      'hover:bg-default-background-info-hover',
+      'hover:text-default-foreground-info-hover'
+    ].join(' ');
+    render(<Button variant='info'>Hello world</Button>);
+
+    expect(screen.getByRole('button', { name: /hello world/i })).toHaveClass(
+      classList
     );
   });
 });
